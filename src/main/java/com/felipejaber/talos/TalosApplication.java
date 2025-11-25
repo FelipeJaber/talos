b99661dd424de507b314c605e2cf6bf9beec9dd8
felipe <felipejaber.dev@gmail.com>
2025-11-25 16:43:34 -0300

    0.0.1 - Init commit

---
+++ src/main/java/com/felipejaber/talos/TalosApplication.java
@@ -0,0 +1,13 @@
+package com.felipejaber.talos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipejaber/talos/TalosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipejaber.talos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
